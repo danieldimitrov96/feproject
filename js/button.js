0b0ed1e24176a09b9de86019e53383b20813cc28
Denis Angelov <de.angelov.bg@gmail.com>
2018-01-25 17:30:36 +0200

    added color and category to newly created  last created note

--- js/button.js
+++ js/button.js
@@ -1,7 +1,13 @@
+
+
 const submitButton = $('#modalSubmitButton');
+const pickColor = $('#colorpicker');
+
+
 submitButton.click(function(e){
-    let noteTitle = $('#noteTitle').val();        
-    let noteText = CKEDITOR.instances.editor1.getData();
+    var noteTitle = $('#noteTitle').val();        
+    var noteText = CKEDITOR.instances.editor1.getData();
+    var noteCathegory = $('#category').val();
     
     //prevent default
     e.preventDefault();
@@ -10,9 +16,17 @@ submitButton.click(function(e){
     $('#addNoteModal').modal('toggle');
 
     //create new note
-    note.add(noteTitle, noteText);
+    console.log(noteCathegory);
+    note.add(noteTitle, noteText, noteCathegory);
+    
+    //set color of the
+    var lastNote=$('.note').last();
+    var colorHEX=pickColor.val();
+    lastNote.css('background-color',colorHEX);
+    
 
     //reset inputs too empty
+
     $('#noteTitle').val('');
     $('#cke_editor1').val('');
     CKEDITOR.instances.editor1.setData('');          
