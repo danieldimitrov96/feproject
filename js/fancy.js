3c4ddf4e5f4f69b5e16d1662c56bedd7e29bdb12
Daniel Dimitrov <Daniel Dimitrov>
2018-01-30 13:16:12 +0200

    fancy js search fixed

--- js/fancy.js
+++ js/fancy.js
@@ -1,12 +1,28 @@
 /* eslint-disable */
 
 // Search nav bar
+let navli = $(".nav-bar-category li");
+
 $("#searchBox").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $(".note-container").each(function () {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-});
+    if (value === '') {
+
+        navli.removeClass("active");
+        $('li').each(function () {
+            let $this = $(this);
+            if ($this.html().indexOf('SHOW ALL') > -1)
+                $this.addClass("active");
+        });
+        $('.note-category-title').each(() => $(this).closest('.note-container').show());
+    } else {
+        $(".note-container").each(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        });
+    };
+})
+
+
+
 
 // smooth scrooling to top on click on the logo
 $('.navbar-brand, .nav-bar-category, #searchBox').on('click', (function () {
@@ -37,36 +53,22 @@ for (let i = 0; i < 3; i++) {
 }
 
 //change active button 
-let navli = $(".nav-bar-category li");
 
 navli.on("click", function () {
     navli.removeClass("active");
     $(this).addClass("active");
+
     let selectedCategory = ($(this).text().trim());
 
     $('.note-category-title').each(function () {
         if (selectedCategory === 'SHOW ALL') {
             $(this).closest('.note-container').show();
             //todo sort 
-
-
-
-
         } else if ($(this).text().trim() === selectedCategory)
             $(this).closest('.note-container').show();
         else {
             $(this).closest('.note-container').hide();
         }
     })
-
-
-
-
-
-
-    //     if (noteClass === 'active') {
-    //     console.log('active');
-    // }
-    // $(this).toggle();
 
 });
