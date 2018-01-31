4942b1fb04f681235a51726f4de860b380cfd972
Simeon <simeonsm@mail.ru>
2018-01-31 14:52:40 +0200

    localStorage background save added.

--- js/load-content.js
+++ js/load-content.js
@@ -1,7 +1,19 @@
 $( document ).ready( function() {
     for (let noteId = 0; noteId < localStorage.length; noteId += 1) {
-        const savedNote = JSON.parse(localStorage.getItem(localStorage.key(noteId)));
+        
+        const noteAsSaved = localStorage.getItem(localStorage.key(noteId));
+        
+        if (noteAsSaved.indexOf('http') > -1) {
+            continue;
+        }
 
-        note.add(savedNote.title, savedNote.content, savedNote.category, savedNote.color, localStorage.key(noteId));
+        const noteParsed = JSON.parse(localStorage.getItem(localStorage.key(noteId)));
+        note.add(noteParsed.title, noteParsed.content, noteParsed.category, noteParsed.color, localStorage.key(noteId));
     }
+
+    if (localStorage.getItem('imgUrl') === null) {
+        localStorage.setItem('imgUrl', 'https://trello-backgrounds.s3.amazonaws.com/SharedBackground/2048x2048/6ca6f4d4009c17936e31ec2977190a25/photo-1517064315897-1b3dc47f0424.jpg');
+    }
+
+    $('body').css('background-image', `url('${localStorage.getItem('imgUrl')}')`);
 } );
